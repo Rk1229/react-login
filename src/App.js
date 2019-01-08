5341d473cde2280b469c90fc8bbb661425370990
Rakesh <rakeshkumart2760@gmail.com>
2019-01-08 23:54:03 +0530

    react app

--- src/App.js
+++ src/App.js
@@ -2,29 +2,79 @@ import React, { Component } from 'react';
 import {
   Container, Col, Form,
   FormGroup, Label, Input,
-  Button,
+  Button, FormText, FormFeedback,
 } from 'reactstrap';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+      'email': '',
+      'password': '',
+      validate: {
+        emailState: '',
+      },
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  validateEmail(e) {
+    const emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const { validate } = this.state
+      if (emailRex.test(e.target.value)) {
+        validate.emailState = 'has-success'
+      } else {
+        validate.emailState = 'has-danger'
+      }
+      this.setState({ validate })
+    }
+
+  handleChange = async (event) => {
+    const { target } = event;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { name } = target;
+    await this.setState({
+      [ name ]: value,
+    });
+  }
+
+  submitForm(e) {
+    e.preventDefault();
+    console.log(`Email: ${ this.state.email }`)
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
       <Container className="App">
         <h2>Sign In</h2>
-        <Form className="form">
+        <Form className="form" onSubmit={ (e) => this.submitForm(e) }>
           <Col>
             <FormGroup>
-              <Label>Email</Label>
+              <Label>Username</Label>
               <Input
                 type="email"
                 name="email"
                 id="exampleEmail"
                 placeholder="myemail@email.com"
+                value={ email }
+                valid={ this.state.validate.emailState === 'has-success' }
+                invalid={ this.state.validate.emailState === 'has-danger' }
+                onChange={ (e) => {
+                            this.validateEmail(e)
+                            this.handleChange(e)
+                          } }
               />
+              <FormFeedback valid>
+                That's a tasty looking email you've got there.
+              </FormFeedback>
+              <FormFeedback>
+                Uh oh! Looks like there is an issue with your email. Please input a correct email.
+              </FormFeedback>
+              <FormText>Your username is most likely your email.</FormText>
             </FormGroup>
           </Col>
-         
           <Col>
             <FormGroup>
               <Label for="examplePassword">Password</Label>
@@ -33,24 +83,13 @@ class App extends Component {
                 name="password"
                 id="examplePassword"
                 placeholder="********"
-              />
-            </FormGroup>
-          </Col>
-          <Col>
-            <FormGroup>
-              <Input
-                type="checkbox"
-                name="checkbox"
-                id="checkbox"
-                required
-              />
-              <br></br>
-              <Label for="checkbox">Remember Me</Label>
-              
+                value={ password }
+                onChange={ (e) => this.handleChange(e) }
+            />
             </FormGroup>
           </Col>
           <Button>Submit</Button>
-        </Form>
+      </Form>
       </Container>
     );
   }
